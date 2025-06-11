a15d7897808e0246f17012584462fd2dcbd5fead
zypherson <taylorzypherson@gmail.com>
2025-06-11 12:02:25 -0400

    Initialized project and setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>Flashcard Quiz</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
