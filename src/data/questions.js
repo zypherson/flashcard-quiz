d892643d91fa23d0614061eaf04b319968e6fdcf
zypherson <taylorzypherson@gmail.com>
2025-06-12 11:57:43 -0400

    added folders/files with questions for flashcards

---
+++ src/data/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Lisbon"],
+      answer: "Paris",
+    },
+    {
+      id: 2,
+      question: "Which planet is known as the Red Planet?",
+      options: ["Earth", "Mars", "Jupiter", "Saturn"],
+      answer: "Mars",
+    },
+    {
+        id: 3,
+        question: "Which location is an US state?",
+        options: ["Colorado", "Florence", "Uganda", "Santa Barbara"],
+        answer: "Colorado",
+      },
+  ];
+  
+  export default questions;
