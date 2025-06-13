809de2cec6e3a2b38f6e68f8f47f7c2fd8fc4089
zypherson <taylorzypherson@gmail.com>
2025-06-13 12:07:48 -0400

    setup flashcard list

---
+++ src/components/FlashcardList.jsx
@@ -0,0 +1,13 @@
+import Flashcard from './Flashcard';
+
+function FlashcardList({ questions }) {
+  return (
+    <div className="flashcard-list">
+      {questions.map((q) => (
+        <Flashcard key={q.id} question={q} />
+      ))}
+    </div>
+  );
+}
+
+export default FlashcardList;
