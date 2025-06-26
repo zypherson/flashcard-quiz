b27eeeac2acb3ea449860fa42b4d1b3347ecb40d
zypherson <taylorzypherson@gmail.com>
2025-06-26 10:57:19 -0400

    added animation as user progresses

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import {motion} from 'framer-motion'
 
 function Flashcard({ question, onAnswer }) {
   const [selected, setSelected] = useState(null);
@@ -10,7 +12,14 @@ function Flashcard({ question, onAnswer }) {
   };
 
   return (
-    <div className="flashcard">
+    
+    <motion.div
+      className="flashcard"
+      initial={{ opacity: 0, x: 50 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -50 }}
+      transition={{ duration: 0.4 }}
+    >
       <h3>{question.question}</h3>
       <ul>
         {question.options.map((option) => (
@@ -31,7 +40,7 @@ function Flashcard({ question, onAnswer }) {
           </li>
         ))}
       </ul>
-    </div>
+      </motion.div>
   );
 }
 
